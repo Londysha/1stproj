37092cdeca5b4f1af1382505f2ee2358d1ce756d
Londysha <daivd.liu.ext@gmail.com>
2023-10-18 10:25:29 -0800

    #

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 from flask import render_template
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import base64
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    series = pd.Series([1, 3, 5, np.nan, 6, 8])
+    figuer = plt.figure()
+    ax = figuer.add_subplot(1, 1, 1)
+    ax.plot(series)
+    plt.savefig(r'./static/images/fig.png')
+    
+    fig=Image.open(r"./static/images/fig.png")
+     # Convert the image to base64
+    image_data = fig.tobytes()
+    base64_image = base64.b64encode(image_data).decode('utf-8')
+
+    return render_template("home.html", fig=base64_image)
 
 
 
